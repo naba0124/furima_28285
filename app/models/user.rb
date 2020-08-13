58cb187e7e8d7068da78c32dd0023c3ed8671de7
naba0124 <68417762+naba0124@users.noreply.github.com>
2020-08-14 00:57:47 +0900

    正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   validates :email, uniqueness: true # アドレスが重複してはいけない
   validates :password, confirmation: true #確認用パスワードと値が一致しなくてはならない
   with_options presence: true do
-    validates :password, format: { with: /\A[a-z0-9]+\z/i } # 半角英数字のみ
-    validates :firstname, :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # 全角ひらがなカタカナ漢字のみ
-    validates :firstname_kana, :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/ } # 全角カタカナのみ
+    validates :password, format: { with: /#{hankaku}/ } # 半角英数字のみ
+    validates :firstname, :lastname, format: { with: /#{zenkaku}/ } # 全角ひらがなカタカナ漢字のみ
+    validates :firstname_kana, :lastname_kana, format: { with: /#{zenkaku_kana}/ } # 全角カタカナのみ
   end
   
   

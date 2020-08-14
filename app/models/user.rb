3a8a3329f64f38f69c559047de96ef0c9cd136c0
naba0124 <68417762+naba0124@users.noreply.github.com>
2020-08-14 10:13:21 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  hankaku = "/\A[a-z0-9]+\z/i"
-  zenkaku = "/\A[ぁ-んァ-ン一-龥]/"
-  zenkaku_kana = "/\A[ァ-ヶー－]+\z/"
   validates :nickname, :email, :password_confirmation, :birthday, :firstname, :lastname, presence: true
   validates :email, uniqueness: true # アドレスが重複してはいけない
   validates :password, confirmation: true #確認用パスワードと値が一致しなくてはならない
   with_options presence: true do
-    validates :password, format: { with: /#{hankaku}/ } # 半角英数字のみ
-    validates :firstname, :lastname, format: { with: /#{zenkaku}/ } # 全角ひらがなカタカナ漢字のみ
-    validates :firstname_kana, :lastname_kana, format: { with: /#{zenkaku_kana}/ } # 全角カタカナのみ
+    validates :password, format: { with: /\A[a-z0-9]+\z/i } # 半角英数字のみ
+    validates :firstname, :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ } # 全角ひらがなカタカナ漢字のみ
+    validates :firstname_kana, :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/ } # 全角カタカナのみ
   end
   
   

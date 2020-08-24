e10e68b52be7d87263d2f61e32618777df1fa3e1
naba0124 <68417762+naba0124@users.noreply.github.com>
2020-08-24 19:25:43 +0900

    バリデーションの設定

--- app/forms/transaction_address.rb
+++ app/forms/transaction_address.rb
@@ -5,7 +5,6 @@ class TransactionAddress
 
   postal = /\A\d{3}[-]\d{4}\z/
   tel = /\A\d{11}\z/
-  validates :address_origin_id, :city, :address, :tel, :post_number, presence: true
   with_options presence: true do
     validates :post_number, format: { with: postal }
     validates :tel, format: { with: tel }
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,14 @@ class Address < ApplicationRecord
   belongs_to :user
 
  
+  
+  validates :address_origin_id, :city, :address, :tel, :post_number, presence: true
+  postal = /\A\d{3}[-]\d{4}\z/
+  tel = /\A\d{11}\z/
+  with_options presence: true do
+    validates :post_number, format: { with: postal }
+    validates :tel, format: { with: tel }
+  end
 
   
 end

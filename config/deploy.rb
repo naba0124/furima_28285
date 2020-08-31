19572cf67ac58c287329c3086d063d3d39f449ba
naba0124 <68417762+naba0124@users.noreply.github.com>
2020-08-31 16:40:50 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,10 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima28285'
+set :application, 'furima_28285'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:nabae0124/furima_28285..git'
+set :repo_url,  'git@github.com:nabae0124/furima_28285.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
